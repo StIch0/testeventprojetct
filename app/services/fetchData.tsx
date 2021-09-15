afaaf439595d742c8b6833baf2f3522247a381ed
pavel <pavel.burdukovskiy@fogstream.ru>
2021-09-15 18:14:54 +1000

    (#main) длобавлен timeout на запрос

--- app/services/fetchData.tsx
+++ app/services/fetchData.tsx
@@ -1,5 +1,29 @@
 import { RequestParams } from './types';
 
+interface ITimeoutProps<T> {
+  ms?: number;
+  promise: Promise<T>;
+}
+
+const timeout = <T,>({ ms = 1000, promise }: ITimeoutProps<T>) => {
+  return new Promise<T>((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Проверьте соединение с интернетом'));
+    }, ms);
+    promise.then(resolve, reject);
+  });
+};
+
+// Код со Stack Overflow
+// export function timeout(ms, promise) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       reject(new Error('timeout'));
+//     }, ms);
+//     promise.then(resolve, reject);
+//   });
+// }
+
 export const fetchData = async <T,>(
   requestParams: RequestParams
 ): Promise<T> => {
@@ -17,7 +41,7 @@ export const fetchData = async <T,>(
       body
     };
 
-    const response = await fetch(url, requestInit);
+    const response = await timeout({ promise: fetch(url, requestInit), ms: 0 });
 
     const responseJSON = (await response.json()) as T;
 
